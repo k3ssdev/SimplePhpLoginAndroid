2dd45ca8d3b4499a91a5730fadf2990506c30a70
k3ssdev <albertoperezdelrio@gmail.com>
2023-10-24 00:32:43 +0200

    added validation controls for input fields

--- app/src/main/java/io/github/k3ssdev/loginformsqliteandroid/MainActivity.java
+++ app/src/main/java/io/github/k3ssdev/loginformsqliteandroid/MainActivity.java
@@ -2,6 +2,7 @@ package io.github.k3ssdev.loginformsqliteandroid;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,6 +24,38 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin_apr.setOnClickListener(v -> {
             String usuario = editTextUsername_apr.getText().toString();
             String contrasena = editTextPassword_apr.getText().toString();
+
+            // Valida que usuario y contraseña no estén vacíos
+            if (usuario.isEmpty() || contrasena.isEmpty()) {
+                // Comprueba que campo esté vacío y muestra un mensaje de error
+                if (usuario.isEmpty()) {
+                    editTextUsername_apr.setError("El usuario no puede estar vacío");
+                }
+                if (contrasena.isEmpty()) {
+                    editTextPassword_apr.setError("La contraseña no puede estar vacía");
+                }
+                // Muestra un mensaje toast
+                Toast.makeText(this, "¡Login incorrecto!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Valida que usuario solo use letras y números
+            if (!usuario.matches("[A-Za-z0-9]+")) {
+                // Muestra un mensaje de error
+                editTextUsername_apr.setError("El usuario solo puede contener letras y números");
+                // Muestra un mensaje toast
+                Toast.makeText(this, "¡Login incorrecto!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Valida que usuario tenga entre 4 y 8 caracteres
+            if (contrasena.length() < 4 || contrasena.length() > 8) {
+                // Muestra un mensaje de error
+                editTextPassword_apr.setError("La contraseña debe tener entre 4 y 8 caracteres");
+                // Muestra un mensaje toast
+                Toast.makeText(this, "¡Login incorrecto!", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
             // Crea una instancia de WebServiceHandler
             WebServiceHandler webServiceHandler = new WebServiceHandler(this);
